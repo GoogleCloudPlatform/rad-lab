458b19d271592860f98e4dc16e04430f93862eee
Johan Euphrosine <proppy@google.com>
2022-03-31 21:52:30 +0900

    modules/silicon_design: add missing license headers

--- modules/silicon_design/scripts/build/build.sh
+++ modules/silicon_design/scripts/build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 # Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the Lpicense.
+# limitations under the License.
 
 set -ex
 
