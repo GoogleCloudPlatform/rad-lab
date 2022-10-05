e56b8e50171d699745a2419cb7225b165eddbdc2
Mukul Gupta <guptamukul@google.com>
2022-10-05 09:30:59 -0700

    Initial Commit

---
+++ modules/neon/scripts/build/startup_script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright 2022 Google LLC
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Create dir for neon notebooks
+echo "Creating directory to store neon notebooks."
+mkdir -p /home/jupyter/neon/notebooks
+
+# Setting environment variabled for Project ID
+echo "Setting Project ID variable."
+export PROJECT_ID=$(gcloud config get-value project)
+
+# Copy neon .pynb to the notebook
+echo "Copying sample notebooks to the instance."
+gsutil -m cp -r gs://user-scripts-${PROJECT_ID}/notebooks/*.ipynb /home/jupyter/neon/notebooks
+
+echo "Startup script finished."
+
+#auto-shutdown script - enable if needed
+
+# wget https://raw.githubusercontent.com/GoogleCloudPlatform/ai-platform-samples/master/notebooks/tools/auto-shutdown/install.sh
+# wget https://raw.githubusercontent.com/GoogleCloudPlatform/ai-platform-samples/master/notebooks/tools/auto-shutdown/ashutdown.service
+# wget https://raw.githubusercontent.com/GoogleCloudPlatform/ai-platform-samples/master/notebooks/tools/auto-shutdown/ashutdown
+
+# ./install.sh
