382f4b1ca74dd5ae681102857245be42c9ae5efc
Mukul Gupta <guptamukul@google.com>
2022-10-13 10:26:56 -0700

    Setting variable type for cromwell_db_name

--- modules/genomics_cromwell/variables.tf
+++ modules/genomics_cromwell/variables.tf
@@ -101,6 +101,7 @@ variable "create_project" {
 
 variable "cromwell_db_name" {
   description = "The name of the SQL Database instance. {{UIMeta group=3 order=1 }}"
+  type        = string
   default     = "cromwelldb"
 }
 
