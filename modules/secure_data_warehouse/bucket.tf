80d765098ae15c12bb9936a7d5e6edbba0df9628
Mukul Gupta <guptamukul@google.com>
2022-12-06 10:33:47 -0800

    Removing duplicate resources

--- modules/secure_data_warehouse/bucket.tf
+++
@@ -1,8 +0,0 @@
-data "google_storage_bucket" "sdw-data-ingest" {
-  name = module.secured_data_warehouse.data_ingestion_bucket_name
-}
-resource "google_storage_bucket_object" "file_upload" {
-  name   = "drivers_license"
-  bucket = module.secured_data_warehouse.data_ingestion_bucket_name
-  source = "${path.module}/dataset/drivers_license.csv"
-}
--- modules/secure_data_warehouse/dataflow.tf
+++ modules/secure_data_warehouse/dataflow.tf
@@ -10,32 +10,32 @@ locals {
   bigquery_confidential_table     = "${module.project_radlab_sdw_conf_data.project_id}:${local.confidential_dataset_id}.${local.confidential_table_id}"
 }
 
-module "regional_deid_pipeline" {
-  source = "GoogleCloudPlatform/secured-data-warehouse/google//modules/dataflow-flex-job"
-  project_id              = module.project_radlab_sdw_data_ingest.project_id
-  name                    = "dataflow-flex-regional-dlp-deid-job-python-query"
-  container_spec_gcs_path = "gs://${data.google_storage_bucket.sdw-data-ingest.name}/${google_storage_bucket_object.template_upload.name}"
-  job_language            = "PYTHON"
-  region                  = local.location
-  service_account_email   = module.secured_data_warehouse.dataflow_controller_service_account_email
-  subnetwork_self_link    = module.dwh_networking_data_ingest.subnets_self_links[0]
-  kms_key_name            = module.secured_data_warehouse.cmek_data_ingestion_crypto_key
-  temp_location           = "gs://${data.google_storage_bucket.sdw-data-ingest.name}/tmp/"
-  staging_location        = "gs://${data.google_storage_bucket.sdw-data-ingest.name}/staging/"
+# module "regional_deid_pipeline" {
+#   source = "GoogleCloudPlatform/secured-data-warehouse/google//modules/dataflow-flex-job"
+#   project_id              = module.project_radlab_sdw_data_ingest.project_id
+#   name                    = "dataflow-flex-regional-dlp-deid-job-python-query"
+#   container_spec_gcs_path = "gs://${data.google_storage_bucket.sdw-data-ingest.name}/${google_storage_bucket_object.template_upload.name}"
+#   job_language            = "PYTHON"
+#   region                  = local.location
+#   service_account_email   = module.secured_data_warehouse.dataflow_controller_service_account_email
+#   subnetwork_self_link    = module.dwh_networking_data_ingest.subnets_self_links[0]
+#   kms_key_name            = module.secured_data_warehouse.cmek_data_ingestion_crypto_key
+#   temp_location           = "gs://${data.google_storage_bucket.sdw-data-ingest.name}/tmp/"
+#   staging_location        = "gs://${data.google_storage_bucket.sdw-data-ingest.name}/staging/"
 
-  parameters = {
-    query                          = "SELECT email, name, street, city, state, zip, dob, dl_id, exp_date FROM [bigquery-public-data:irs_990.irs_990_ein] LIMIT 10000"
-    deidentification_template_name = module.de_identification_template.template_full_path
-    window_interval_sec            = 30
-    batch_size                     = 1000
-    dlp_location                   = local.location
-    dlp_project                    = module.project_radlab_sdw_data_govern.project_id
-    bq_schema                      = local.bq_schema_irs_990_ein
-    output_table                   = local.bigquery_confidential_table
-    dlp_transform                  = "RE-IDENTIFY"
+#   parameters = {
+#     query                          = "SELECT email, name, street, city, state, zip, dob, dl_id, exp_date FROM [bigquery-public-data:irs_990.irs_990_ein] LIMIT 10000"
+#     deidentification_template_name = module.de_identification_template.template_full_path
+#     window_interval_sec            = 30
+#     batch_size                     = 1000
+#     dlp_location                   = local.location
+#     dlp_project                    = module.project_radlab_sdw_data_govern.project_id
+#     bq_schema                      = local.bq_schema_irs_990_ein
+#     output_table                   = local.bigquery_confidential_table
+#     dlp_transform                  = "RE-IDENTIFY"
 
-  }
-}
+#   }
+# }
 
 
 /*module "regional_reid_pipeline" {
@@ -64,6 +64,7 @@ module "regional_deid_pipeline" {
     dlp_transform                  = "RE-IDENTIFY"
   }
 }*/
+
 /*resource "google_dataflow_job" "regional_deid" {
  project            = module.project_radlab_sdw_data_ingest.project_id
   name              = "dataflow-job"
