9901044356a33f2b92057ad33c6fe05fb6ab3883
Alan Colver <acolver@google.com>
2022-12-07 14:19:42 -0700

    Fix error

---
+++ radlab-ui/webapp/cypress/support/commands.ts
@@ -0,0 +1,6 @@
+// https://docs.cypress.io/api/cypress-api/custom-commands
+export const foo = "bar"
+
+// Cypress.Commands.add("clickLink", (label: string) => {
+//   cy.get("a").contains(label).click()
+// })
