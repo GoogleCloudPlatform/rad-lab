e4f5fc574657d29cf41084f59c6ad3ce63decee4
Mukul Gupta <guptamukul@google.com>
2023-03-07 11:44:52 -0800

    Fix for missing mandatory constraints on SDW module (#31)
    * SDW Mandatory fields are marked as mandatory
    
    * Update package-lock.json
    
    * update simplify logic

--- radlab-ui/webapp/src/utils/terraform.ts
+++ radlab-ui/webapp/src/utils/terraform.ts
@@ -113,7 +113,7 @@ const mapHclToUIVar = (
       ? false
       : null,
     // In TF, desription = "" is how we say it's optional
-    required: hclVar.default ? hclVar.default !== "" : false,
+    required: typeof hclVar.default === "undefined" || hclVar.default === "",
     group,
     order,
     options,
