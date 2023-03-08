17fd9ceb7240b9ab0055cd28d83cce4418e2c43e
Alan Colver <acolver@google.com>
2023-03-08 12:20:03 -0700

    Disable SWC

--- radlab-ui/webapp/next.config.js
+++ radlab-ui/webapp/next.config.js
@@ -4,6 +4,7 @@ const baseConfig = {
   poweredByHeader: false,
   trailingSlash: true,
   basePath: "",
+  swcMinify: false,
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
