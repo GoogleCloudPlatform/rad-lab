4dba086221867bb18dae625b745a83db427cc5ce
Alan Colver <acolver@google.com>
2023-03-08 13:41:08 -0700

    Add experimental SWC flag to Next config

--- radlab-ui/webapp/next.config.js
+++ radlab-ui/webapp/next.config.js
@@ -5,6 +5,9 @@ const baseConfig = {
   trailingSlash: true,
   basePath: "",
   swcMinify: false,
+  experimental: {
+    swcFileReading: false,
+  },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
