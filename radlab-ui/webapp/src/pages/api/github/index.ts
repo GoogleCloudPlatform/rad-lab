6794a6b0bf600541fea552fae0bdcdb9d27c375c
Sachin <sogoduraju@google.com>
2023-09-27 16:21:19 +0000

    API to fetch modules data from git hub

---
+++ radlab-ui/webapp/src/pages/api/github/index.ts
@@ -0,0 +1,33 @@
+import { getGitHubModulesData, getSecretKeyValue } from "@/utils/api"
+import { envOrFail } from "@/utils/env"
+import { withAuth } from "@/utils/middleware"
+import { AuthedNextApiHandler } from "@/utils/types"
+import { NextApiResponse } from "next"
+
+const GIT_TOKEN_SECRET_KEY_NAME = envOrFail(
+  "GIT_TOKEN_SECRET_KEY_NAME",
+  process.env.GIT_TOKEN_SECRET_KEY_NAME,
+)
+const getModuleDataFromGitHub = async (
+  req: AuthedNextApiHandler,
+  res: NextApiResponse,
+) => {
+  const { path } = req.body
+  const secret = await getSecretKeyValue(GIT_TOKEN_SECRET_KEY_NAME)
+  try {
+    const modules = await getGitHubModulesData(path, secret)
+    return res.status(200).json(modules)
+  } catch (error: any) {
+    return res.status(error.response.status).json({ message: error.message })
+  }
+}
+
+const handler = async (req: AuthedNextApiHandler, res: NextApiResponse) => {
+  try {
+    if (req.method === "POST") return getModuleDataFromGitHub(req, res)
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" })
+  }
+}
+
+export default withAuth(handler)
